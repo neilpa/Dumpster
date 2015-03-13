68cd6b35f902f5b8f354eeb3783467af2864eb35
Neil Pankey <npankey@gmail.com>
2015-03-12 19:52:47 -0700

    Initial commit

---
+++ Queue/Queue.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Queue.
+extern double QueueVersionNumber;
+
+/// Project version string for Queue.
+extern const unsigned char QueueVersionString[];
