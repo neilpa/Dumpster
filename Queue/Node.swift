4647723246e8ca028418ec76c0bd6f77c1b5f261
Neil Pankey <npankey@gmail.com>
2015-03-13 10:46:07 -0700

    Node conforms to SequenceType

--- Queue/Node.swift
+++ Queue/Node.swift
@@ -3,9 +3,23 @@
 /// Singly-linked list of elements in `Queue`
 internal final class Node<T> {
     internal let value: T
-    internal var next: Node<T>?
+    internal var next: Node?
 
     internal init(_ value: T) {
         self.value = value
+    }
+}
+
+extension Node : SequenceType {
+    typealias Generator = GeneratorOf<Node>
+
+    func generate() -> Generator {
+        var node: Node? = self
+
+        return Generator {
+            let current = node
+            node = current?.next
+            return current
+        }
     }
 }
