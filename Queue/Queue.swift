a6e7bfe8c6cfdc6b802fe11cf07b3e1b20a070b8
Neil Pankey <npankey@gmail.com>
2015-03-12 21:24:18 -0700

    Documentation comments

--- Queue/Queue.swift
+++ Queue/Queue.swift
@@ -1,12 +1,15 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 
+/// Generic FIFO queue
 public struct Queue<T> : QueueType {
     private var head: Node<T>?
     private var tail: Node<T>?
 
+    /// Initializes an empty `Queue`
     public init() {
     }
 
+    /// Appends an element to the end of `Queue`
     public mutating func enqueue(value: T) {
         let tail = Node(value)
         if head == nil {
@@ -17,17 +20,20 @@ public struct Queue<T> : QueueType {
         self.tail = tail
     }
 
+    /// Removes the first element in `Queue` and returns it, `nil` if empty.
     public mutating func dequeue() -> T? {
         let value = peek()
         head = head?.next
         return value
     }
 
+    /// Returns the element at the head of `Queue`, or `nil` if empty.
     public func peek() -> T? {
         return head?.value
     }
 }
 
+/// Singly-linked list of elements in the queue
 private final class Node<T> {
     let value: T
     var next: Node<T>?
--- Queue/QueueType.swift
+++ Queue/QueueType.swift
@@ -1,11 +1,16 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 
+/// Protocol for a FIFO data structure
 public protocol QueueType {
+    /// Value type stored in `QueueType`
     typealias Element
 
+    /// Appends an element to the end of `QueueType`
     mutating func enqueue(element: Element)
 
+    /// Removes the first element in `QueueType` and returns it, `nil` if empty.
     mutating func dequeue() -> Element?
 
+    /// Returns the first element in `QueueType`, `nil` if empty.
     func peek() -> Element?
 }
