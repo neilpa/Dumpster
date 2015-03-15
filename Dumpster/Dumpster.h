2e2976701f3542147287d705787a4a4ffc92f3ca
Neil Pankey <npankey@gmail.com>
2015-03-14 19:00:35 -0700

    Dumpster :fire:

---
+++ Dumpster/Dumpster.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Dumpster.
+extern double DumpsterVersionNumber;
+
+/// Project version string for Dumpster.
+extern const unsigned char DumpsterVersionString[];
--- Queue/Queue.h
+++
@@ -1,7 +0,0 @@
-//  Copyright (c) 2015 Neil Pankey. All rights reserved.
-
-/// Project version number for Queue.
-extern double QueueVersionNumber;
-
-/// Project version string for Queue.
-extern const unsigned char QueueVersionString[];
